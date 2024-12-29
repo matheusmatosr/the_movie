002337b2f219c803cedc2d3aa9478aabd9604ea9
Matheus Matos <100291312+matheusmatosr@users.noreply.github.com>
2024-12-29 08:25:07 -0300

    fix: ajustes estilização

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -20,7 +20,7 @@ function Home() {
   return (
     <div className="home-container">
       <div className="header">
-        <h1 className="title">Explore filmes e séries.</h1>
+        <h1 className="title-home">Explore filmes e séries.</h1>
         <Search searchValue={searchValue} setSearchValue={setSearchValue} />
       </div>
       <MovieList
